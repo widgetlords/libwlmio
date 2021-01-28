edb2c142780c87ceda4765dc25609ab914e3ef85
Kelsey Maes <kelsey@vpprocess.com>
2021-01-28 10:30:28 -0800

    Clean up some formatting issues

--- src/libwlmio/wlmio.h
+++ src/libwlmio/wlmio.h
@@ -10,32 +10,32 @@ extern "C" {
 #if 0
 enum wlmio_transfer
 {
-	WLMIO_TRANSFER_MESSAGE = 0,
-	WLMIO_TRANSFER_RESPONSE = 1,
-	WLMIO_TRANSFER_REQUEST = 2
+  WLMIO_TRANSFER_MESSAGE = 0,
+  WLMIO_TRANSFER_RESPONSE = 1,
+  WLMIO_TRANSFER_REQUEST = 2
 };
 
 enum wlmio_priority
 {
-	WLMIO_PRIORITY_EXCEPTIONAL = 0,
-	WLMIO_PRIORITY_IMMEDIATE = 1,
-	WLMIO_PRIORITY_FAST = 2,
-	WLMIO_PRIORITY_HIGH = 3,
-	WLMIO_PRIORITY_NOMINAL = 4,
-	WLMIO_PRIORITY_LOW = 5,
-	WLMIO_PRIORITY_SLOW = 6,
-	WLMIO_PRIORITY_OPTIONAL = 7
+  WLMIO_PRIORITY_EXCEPTIONAL = 0,
+  WLMIO_PRIORITY_IMMEDIATE = 1,
+  WLMIO_PRIORITY_FAST = 2,
+  WLMIO_PRIORITY_HIGH = 3,
+  WLMIO_PRIORITY_NOMINAL = 4,
+  WLMIO_PRIORITY_LOW = 5,
+  WLMIO_PRIORITY_SLOW = 6,
+  WLMIO_PRIORITY_OPTIONAL = 7
 };
 
 struct wlmio_transfer
 {
-	uint64_t timestamp_usec;
-	uint8_t priority;
-	uint8_t transfer_type;
-	uint16_t port_id;
-	uint8_t remote_node_id;
-	size_t payload_size;
-	void* payload;
+  uint64_t timestamp_usec;
+  uint8_t priority;
+  uint8_t transfer_type;
+  uint16_t port_id;
+  uint8_t remote_node_id;
+  size_t payload_size;
+  void* payload;
 };
 #endif
 
@@ -66,28 +66,28 @@ struct wlmio_status
 
 enum wlmio_register_value
 {
-	WLMIO_REGISTER_VALUE_EMPTY = 0U,
-	WLMIO_REGISTER_VALUE_STRING = 1U,
-	WLMIO_REGISTER_VALUE_UNSTRUCTURED = 2U,
-	WLMIO_REGISTER_VALUE_BIT = 3U,
-	WLMIO_REGISTER_VALUE_INT64 = 4U,
-	WLMIO_REGISTER_VALUE_INT32 = 5U,
-	WLMIO_REGISTER_VALUE_INT16 = 6U,
-	WLMIO_REGISTER_VALUE_INT8 = 7U,
-	WLMIO_REGISTER_VALUE_UINT64 = 8U,
-	WLMIO_REGISTER_VALUE_UINT32 = 9U,
-	WLMIO_REGISTER_VALUE_UINT16 = 10U,
-	WLMIO_REGISTER_VALUE_UINT8 = 11U,
-	WLMIO_REGISTER_VALUE_FLOAT64 = 12U,
-	WLMIO_REGISTER_VALUE_FLOAT32 = 13U,
-	WLMIO_REGISTER_VALUE_FLOAT16 = 14U
+  WLMIO_REGISTER_VALUE_EMPTY = 0U,
+  WLMIO_REGISTER_VALUE_STRING = 1U,
+  WLMIO_REGISTER_VALUE_UNSTRUCTURED = 2U,
+  WLMIO_REGISTER_VALUE_BIT = 3U,
+  WLMIO_REGISTER_VALUE_INT64 = 4U,
+  WLMIO_REGISTER_VALUE_INT32 = 5U,
+  WLMIO_REGISTER_VALUE_INT16 = 6U,
+  WLMIO_REGISTER_VALUE_INT8 = 7U,
+  WLMIO_REGISTER_VALUE_UINT64 = 8U,
+  WLMIO_REGISTER_VALUE_UINT32 = 9U,
+  WLMIO_REGISTER_VALUE_UINT16 = 10U,
+  WLMIO_REGISTER_VALUE_UINT8 = 11U,
+  WLMIO_REGISTER_VALUE_FLOAT64 = 12U,
+  WLMIO_REGISTER_VALUE_FLOAT32 = 13U,
+  WLMIO_REGISTER_VALUE_FLOAT16 = 14U
 };
 
 struct wlmio_register_access
 {
-	uint8_t type;
-	uint16_t length;
-	uint8_t value[256];
+  uint8_t type;
+  uint16_t length;
+  uint8_t value[256];
 };
 
 enum wlmio_command
@@ -113,33 +113,33 @@ enum wlmio_command_status
 
 struct wlmio_node_version
 {
-	uint8_t major;
-	uint8_t minor;
+  uint8_t major;
+  uint8_t minor;
 };
 
 enum wlmio_node_info_flags
 {
-	WLMIO_NODE_INFO_CRC = 1,
-	WLMIO_NODE_INFO_CERTIFICATE_OF_AUTHENTICITY = 2
+  WLMIO_NODE_INFO_CRC = 1,
+  WLMIO_NODE_INFO_CERTIFICATE_OF_AUTHENTICITY = 2
 };
 
 struct wlmio_node_info
 {
-	struct wlmio_node_version protocol_version;
-	struct wlmio_node_version hardware_version;
-	struct wlmio_node_version software_version;
-	uint64_t software_vcs_revision_id;
-	uint8_t unique_id[16];
-	char name[51];
-	uint64_t software_image_crc;
-	uint8_t certificate_of_authenticity[222];
-	uint32_t flags;
+  struct wlmio_node_version protocol_version;
+  struct wlmio_node_version hardware_version;
+  struct wlmio_node_version software_version;
+  uint64_t software_vcs_revision_id;
+  uint8_t unique_id[16];
+  char name[51];
+  uint64_t software_image_crc;
+  uint8_t certificate_of_authenticity[222];
+  uint32_t flags;
 };
 
 
 /**
  * Initializes the library
- * 
+ *
  * @return Returns 0 if success else -1
 */
 int32_t wlmio_init(void);
@@ -147,7 +147,7 @@ int32_t wlmio_init(void);
 
 /**
  * Executes all currently pending events and returns
- * 
+ *
  * @return Returns 0 if success else -1
 */
 int32_t wlmio_tick(void);
@@ -164,7 +164,7 @@ uint8_t wlmio_get_node_id(void);
 
 /**
  * List the registers present on a node one at a time.
- * 
+ *
  * @param name The name pointer must point to an array of at least 51 bytes
 */
 int32_t wlmio_register_list(uint8_t node_id, uint16_t index, char* name, void (* callback)(int32_t r, void* uparam), void* uparam);
@@ -181,12 +181,12 @@ int32_t wlmio_node_set_sample_interval(uint8_t node_id, uint16_t sample_interval
 
 struct wlmio_vpe6010_input
 {
-	uint16_t ma_5v;
-	uint16_t mv_5v;
-	uint16_t mv_24v1;
-	uint16_t mv_24v2;
-	uint16_t mv_24v;
-	uint16_t ma_24v;
+  uint16_t ma_5v;
+  uint16_t mv_5v;
+  uint16_t mv_24v1;
+  uint16_t mv_24v2;
+  uint16_t mv_24v;
+  uint16_t ma_24v;
 };
 int32_t wlmio_vpe6010_read(uint8_t node_id, struct wlmio_vpe6010_input* dst, void (* callback)(int32_t r, void* uparam), void* uparam);
 
@@ -194,31 +194,31 @@ int32_t wlmio_vpe6030_write(uint8_t node_id, uint8_t ch, uint8_t v, void (* call
 
 enum wlmio_vpe6040
 {
-	WLMIO_VPE6040_MODE_5V = 0,
-	WLMIO_VPE6040_MODE_MA = 1,
-	WLMIO_VPE6040_MODE_10V = 2
+  WLMIO_VPE6040_MODE_5V = 0,
+  WLMIO_VPE6040_MODE_MA = 1,
+  WLMIO_VPE6040_MODE_10V = 2
 };
 int32_t wlmio_vpe6040_read(uint8_t node_id, uint8_t ch, uint16_t* v, void (* callback)(int32_t r, void* uparam), void* uparam);
 int32_t wlmio_vpe6040_configure(uint8_t node_id, uint8_t ch, uint8_t mode, void (* callback)(int32_t r, void* uparam), void* uparam);
 
 enum wlmio_vpe6050
 {
-	WLMIO_VPE6050_MODE_SOURCE = 0,
-	WLMIO_VPE6050_MODE_SINK = 1
+  WLMIO_VPE6050_MODE_SOURCE = 0,
+  WLMIO_VPE6050_MODE_SINK = 1
 };
 int32_t wlmio_vpe6050_write(uint8_t node_id, uint8_t ch, uint16_t v, void (* callback)(int32_t r, void* uparam), void* uparam);
 int32_t wlmio_vpe6050_configure(uint8_t node_id, uint8_t ch, uint8_t mode, void (* callback)(int32_t r, void* uparam), void* uparam);
 
 enum wlmio_vpe6060
 {
-	WLMIO_VPE6060_MODE_BASIC = 0,
-	WLMIO_VPE6060_MODE_FREQUENCY = 1,
-	WLMIO_VPE6060_MODE_PULSE_COUNTER = 2,
-	WLMIO_VPE6060_BIAS_NONE = 0,
-	WLMIO_VPE6060_BIAS_PNP = 1,
-	WLMIO_VPE6060_BIAS_NPN = 2,
-	WLMIO_VPE6060_POLARITY_RISING = 0,
-	WLMIO_VPE6060_POLARITY_FALLING = 1
+  WLMIO_VPE6060_MODE_BASIC = 0,
+  WLMIO_VPE6060_MODE_FREQUENCY = 1,
+  WLMIO_VPE6060_MODE_PULSE_COUNTER = 2,
+  WLMIO_VPE6060_BIAS_NONE = 0,
+  WLMIO_VPE6060_BIAS_PNP = 1,
+  WLMIO_VPE6060_BIAS_NPN = 2,
+  WLMIO_VPE6060_POLARITY_RISING = 0,
+  WLMIO_VPE6060_POLARITY_FALLING = 1
 };
 int32_t wlmio_vpe6060_read(uint8_t node_id, uint8_t ch, uint32_t* v, void (* callback)(int32_t r, void* uparam), void* uparam);
 int32_t wlmio_vpe6060_configure(uint8_t node_id, uint8_t ch, uint8_t mode, uint8_t polarity, uint8_t bias, void (* callback)(int32_t r, void* uparam), void* uparam);
@@ -233,9 +233,9 @@ int32_t wlmio_vpe6080_configure(uint8_t node_id, uint8_t ch, uint8_t enabled, ui
 
 /**
  * List the registers present on a node one at a time.
- * 
+ *
  * Synchronous equivalent of wlmio_register_list()
- * 
+ *
  * @param name The name pointer must point to an array of at least 51 bytes
 */
 int32_t wlmio_register_list_sync(uint8_t node_id, uint16_t index, char* name);
